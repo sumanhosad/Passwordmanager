61e06b7402049cb8ed6d567b3a705d6ee51a46e1
sumanhosad <sumanhosad98@gmail.com>
2024-04-05 12:00:01 +0530

    databse is gettting made

--- website/app.py
+++ website/app.py
@@ -10,40 +10,46 @@ class User(db.Model):
     username = db.Column(db.String(50), unique=True, nullable=False)
     master_password = db.Column(db.String(100), nullable=False)
 
-class Password(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    sitename = db.Column(db.String(100), nullable=False)
-    url = db.Column(db.String(200), nullable=False)
-    username = db.Column(db.String(50), nullable=False)
-    password = db.Column(db.String(100), nullable=False)
-
-    user = db.relationship('User', backref=db.backref('passwords', lazy=True))
-
-def init_db(app):
-    db.init_app(app)
-    with app.app_context():
-        db.create_all()
-
+def create_passwords_table(username):
+    class_name = f'{username.capitalize()}Password'
+    return type(class_name, (db.Model,), {
+        '__tablename__': f'{username}_passwords',
+        'id': db.Column(db.Integer, primary_key=True),
+        'urlname': db.Column(db.String(100), nullable=False),
+        'url': db.Column(db.String(200), nullable=False),
+        'username': db.Column(db.String(50), nullable=False),
+        'password': db.Column(db.String(100), nullable=False),
+        'user_id': db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False),
+        'user': db.relationship(User)
+    })
 def create_app():
+
+
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'passwordmanager'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///passwordmanager.db'
 
-    init_db(app)
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
+
 
     @app.route('/login', methods=["POST","GET"])
     def login():
         session.pop("user", None)
         if request.method == "POST":
             user = request.form["username"]
-            master_password=request.form["password"]
-            session["user"] = user
-            return redirect(url_for("home", usr=user))
+            master_password = request.form["password"]
+            existing_user = User.query.filter_by(username=user).first()
+            if existing_user and existing_user.master_password == hash_password(master_password):
+                session["user"] = user
+                return redirect(url_for("home", usr=user))
+            else:
+                return redirect(url_for("login"))
+
         else:
             session.clear()
             return render_template('login.html')
-
     @app.route('/')
     def home():
         if "user" in session:
@@ -65,6 +71,8 @@ def create_app():
             
             session["user"] = user
             useradd(user, hashed_mp)
+            create_passwords_table(user)
+            db.create_all()
             return redirect(url_for("home", usr=user))
         else:
             return render_template('signin.html')
